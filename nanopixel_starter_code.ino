bcb87ee73a716882e89cae68913f795d9536b4bf
Ponderrawr <ponderrawr@gmail.com>
2025-09-13 17:14:04 -0400

    Add files via upload

---
+++ nanopixel_starter_code.ino
@@ -0,0 +1,34 @@
+#include <FastLED.h>
+#define NUM_LEDS 7
+#define LED_PIN 10
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  // put your setup code here, to run once:
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(50);
+  delay(2000);
+}
+
+void loop () {
+  for (int i = 0; i < NUM_LEDS; i++) {
+   leds[i] = CRGB::Red;
+   FastLED.show();
+  } 
+
+  for (int i = NUM_LEDS-1; i >= 0; i--) {
+   leds[i] = CRGB::Green;
+   FastLED.show();
+  }
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+   leds[i] = CRGB::Blue;
+   FastLED.show();
+  }
+
+  for (int i = NUM_LEDS-1; i >= 0; i--) {
+   leds[i] = CRGB::Cyan;
+   FastLED.show();
+  }
+ }
